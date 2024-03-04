0eb566c076fe5d4cf96a1c45d8863d06b8b3a39f
Haritha <Harimigara@gmail.com>
2024-03-04 14:31:23 +0530

    add delete option

--- src/components/viewTodo/ApiData.tsx
+++ src/components/viewTodo/ApiData.tsx
@@ -1,6 +1,7 @@
 // ApiData.tsx
 import React, { useState, useEffect } from 'react';
 import './apiData.css';
+import '../addTodo/addtodo.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 interface DataItem {
@@ -9,11 +10,14 @@ interface DataItem {
   discription: string;
   setDate:String;
   endDate:String;
-
 }
 
 const ApiData: React.FC = () => {
   const [data, setData] = useState<DataItem[]>([]);
+  const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [startDate, setStartDate] = useState<string>('');
+  const [endDate, setEndDate] = useState<string>('');
 
     useEffect(() => {
     fetchData();
@@ -34,6 +38,7 @@ const ApiData: React.FC = () => {
     }
   };
 
+    // delete selected todo from the API
     const handleDelete = async (id: number) => {
     try {
       const response = await fetch(`https://localhost:44315/api/Todos/${id}`, {
@@ -53,39 +58,152 @@ const ApiData: React.FC = () => {
     }
   };
 
+  //Lord all the TODOs to the table in the start
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const newItem: DataItem = {
+      id: 0, // Generating id dynamically
+      titel: title,
+      discription: description,
+      setDate: startDate,
+      endDate: endDate
+    };
+
+    try {
+      const response = await fetch('https://localhost:44315/api/Todos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newItem),
+      });
+
+      if (response.ok) {
+        console.log('Data added successfully');
+        window.location.reload();
+        // Clear the form fields after successful submission
+        setTitle('');
+        setDescription('');
+        setStartDate('');
+        setEndDate('');
+      } else {
+        console.error('Failed to add data');
+      }
+    } catch (error) {
+      console.error('Error occurred:', error);
+    }
+  };
+
+  // //Update TODO with API
+  // const UpdateData = async (data item) => {
+
+  // }
 
   return (
-    <div className="container">
-  <h1>Available Todos</h1>
-  <table className="data-table table table-success table-striped">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Title</th>
-        <th>Description</th>
-        <th>Set Date</th>
-        <th>End Date</th>
-        <th>Options</th>
-      </tr>
-    </thead>
-    <tbody>
-      {data.map(item => (
-        <tr key={item.id}>
-          <td>{item.id}</td>
-          <td>{item.titel}</td>
-          <td>{item.discription}</td>
-          <td>{item.setDate.split("T")[0]}</td>
-          <td>{item.endDate.split( "T" )[0]}</td>
-          <td>
-            <button type="button" className="btn-edit btn btn-primary">Edit</button>
-            <button type="button" className="btn btn-danger" onClick={() => handleDelete(item.id)}>Delete</button>
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-</div>
+    <>
 
+    <section className='add-data-section'>
+      <div className='container justify-content-center mt-5'>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3 row">
+          <label htmlFor="todoTitle" className="col-sm-2 col-form-label form-label">Todo Title</label>
+          <div className="col-sm-10">
+            <input 
+              type="text" 
+              className="form-control form-input" 
+              id="todoTitle" 
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+          </div>
+        </div>
+
+        <div className="mb-3 row">
+          <label htmlFor="todoDescription" className="col-sm-2 col-form-label form-label">Todo Description</label>
+          <div className="col-sm-10">
+            <input 
+              type="text" 
+              className="form-control form-input" 
+              id="todoDescription" 
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+            />
+          </div>
+        </div>
+
+        <div className="mb-3 row">
+          <label htmlFor="startDate" className="col-sm-2 col-form-label form-label">Todo Start Date</label>
+          <div className="col-sm-10">
+            <input 
+              type="date" 
+              className="form-control form-input" 
+              id="startDate" 
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+              required
+            />
+          </div>
+        </div>
+
+        <div className="mb-3 row">
+          <label htmlFor="endDate" className="col-sm-2 col-form-label form-label">Todo End Date</label>
+          <div className="col-sm-10">
+            <input 
+              type="date" 
+              className="form-control form-input" 
+              id="endDate" 
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+              required
+            />
+          </div>
+        </div>
+
+        <div className="mb-3 row">
+          <div className="col-sm-10 offset-sm-2">
+            <button type="submit" className="btn-add btn btn-success">Add new Todo</button>
+          </div>
+        </div>
+      </form>
+    </div>
+    </section>
+
+<section className='view-data-section'>
+    <div className="container">
+      <h1>Available Todos</h1>
+      <table className="data-table table table-success table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Set Date</th>
+            <th>End Date</th>
+            <th>Options</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(item => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.titel}</td>
+              <td>{item.discription}</td>
+              <td>{item.setDate.split("T")[0]}</td>
+              <td>{item.endDate.split( "T" )[0]}</td>
+              <td>
+                <button type="button" className="btn-edit btn btn-primary" onClick={() =>UpdateData(item)} >Update</button>
+                <button type="button" className="btn btn-danger" onClick={() => handleDelete(item.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+</section>
+</>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import ApiData from './components/viewTodo/ApiData.tsx'
-import AddTodo from './components/addTodo/AddTodo.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AddTodo/>
     <ApiData />
   </React.StrictMode>,
 )
